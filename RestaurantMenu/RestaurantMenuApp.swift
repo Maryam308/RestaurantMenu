ef3a46fc0f30883557076e3f651b46112bb0bea1
Maryam308 <mariam601me@gmail.com>
2024-09-16 11:23:32 +0300

    Initial Commit

---
+++ RestaurantMenu/RestaurantMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuApp.swift
+//  RestaurantMenu
+//
+//  Created by Maryam Yousif on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
